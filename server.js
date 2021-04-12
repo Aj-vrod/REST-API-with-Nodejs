ff3bee69008d7435feb9e5c958df106a21e7599f
Ammy_Varela <ammyvrodriguez@hotmail.com>
2021-04-12 20:46:40 +0200

    added first draft of sql

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 // USING EXPRESS INSTEAD OF HTTP
 const express = require('express')
 const app = express()
+const path = require('path')
+const db = require(path.join(__dirname, 'db', 'database.js'))
 
 const port = 3000
 app.listen(port, () => {
@@ -8,8 +10,19 @@ app.listen(port, () => {
 })
 
 // GET ROOMS
- app.get('/rest/rooms', (req, res) => {
-  res.json({"message": "Ok"})
+ app.get('/rest/users', (req, res) => {
+  const query = 'select * from user'
+  var params = []
+  db.all(query, params, (error, rows) => {
+    if (error) {
+      res.status(400).json({"error": error.message });
+      return;
+    }
+    res.json({
+      "message": "success",
+      "data": rows
+    })
+  });
  });
 
 // DEFAULT RESPONSE FOR ERRORS
