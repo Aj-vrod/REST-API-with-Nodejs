322b28a48c8e7f49b767637fe804523130869367
Ammy_Varela <ammyvrodriguez@hotmail.com>
2021-04-13 17:02:08 +0200

    added GET bookings + booking

--- server.js
+++ server.js
@@ -68,6 +68,32 @@ app.get('/rest/seats/:id', (req, res) => {
 
 })
 
+// GET BOOKINGS
+app.get('/rest/bookings', (req, res) => {
+  const query = 'SELECT * FROM bookings'
+  params = []
+  db.all(query, params, (err, rows) => {
+    if (err) {
+      res.status(400).json({"error": err.message})
+      return
+    }
+    res.status(200).json(rows)
+  })
+})
+
+// GET BOOKING
+app.get('/rest/booking', (req, res) => {
+  const query = `SELECT id, (SELECT json_object('id', id, (SELECT json_object('id', id, 'name', name) FROM rooms WHERE rooms.id = seats.room) AS room) AS seat), date, (SELECT json_object('id', id, 'name', name, 'profilePicture', profilePicture) FROM users WHERE bookings.user = users.id) AS user FROM bookings`
+  const params = [req.params.id]
+  db.get(query, params, (err, row) => {
+    if (err) {
+      res.status(400).json({"error": err.message})
+      return
+    }
+    res.status(200).json(row)
+  })
+})
+
 // DEFAULT RESPONSE FOR ERRORS
 app.use((req, res) => {
   res.status(404);
