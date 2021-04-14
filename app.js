0321e7380ddadd730b971814de87dd9c738a6350
Ammy_Varela <ammyvrodriguez@hotmail.com>
2021-04-14 21:53:37 +0200

    added first code for session authorization

--- app.js
+++ app.js
@@ -3,11 +3,46 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const db = require(path.join(__dirname, 'db', 'database'))
+const jwt = require('jsonwebtoken')
 
 // USING BODY-PARSER TO PARSE BODY DATA FROM REQUESTS
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+// SESSION ROUTES
+app.get('/rest', (req, res) => {
+  res.json({
+    "message": "Welcome to Amiconsult Office API"
+  })
+})
+
+const login = (req, res) => {
+  const query = 'SELECT name FROM users WHERE name = ?'
+  params = [req.headers['name']]
+  db.all(query, params, (err, user) => {
+    if (err) {
+      res.status(400).json(err.message)
+      return
+    } else if (user.length) {
+      jwt.sign({user: user}, 'secretToken', (err, token) => {
+        res.json({
+          token,
+        })
+      })
+    } else {
+      res.status(403).json({
+        "message": "403 No authorized access"
+      })
+    }
+  })
+}
+
+const verifyUser = () => {
+
+}
+
+app.post('/rest/login', login)
 
 // ROOMS ROUTES
 const roomsController = require('./controllers/rooms.controller')
