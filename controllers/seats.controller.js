02189453158f8aae586e7f2ff60f4836f2fdca15
Ammy_Varela <ammyvrodriguez@hotmail.com>
2021-04-14 14:59:01 +0200

    typo corrected

--- controllers/seats.controller.js
+++ controllers/seats.controller.js
@@ -4,7 +4,7 @@ const checkErrors = require('../helpers/errors')
 const index = (req, res) => {
   const query = `SELECT id, (SELECT json_object('id', id, 'name', name) FROM rooms WHERE rooms.id = seats.room) AS room FROM seats`
   const params = []
-  db.all(query, params, (error, rows) => {
+  db.all(query, params, (err, rows) => {
     checkErrors(err, res)
     res.status(200).json(rows)
   })
@@ -13,7 +13,7 @@ const index = (req, res) => {
 const show = (req, res) => {
   const params = [req.params.id]
   const query = `SELECT id, (SELECT json_object('id', id, 'name', name) FROM rooms WHERE rooms.id = seats.room) AS room FROM seats WHERE id = ?`
-  db.get(query, params, (error, row) => {
+  db.get(query, params, (err, row) => {
     checkErrors(err, res)
     res.status(200).json(row)
   })
