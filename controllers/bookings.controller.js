668d5052e0e2c490cc32a5dd2b50b38cf5e89c6f
Ammy_Varela <ammyvrodriguez@hotmail.com>
2021-04-14 18:07:33 +0200

    changed error message

--- controllers/bookings.controller.js
+++ controllers/bookings.controller.js
@@ -77,7 +77,7 @@ const create = (req, res) => {
   const params = [data.seat, data.date, data.user]
   db.run(query, params, (err, result) => {
     if (err) {
-      res.status(400).json({"error": "A database error occurred"})
+      res.status(500).json({"error": "A database error occurred"})
       return;
     }
     res.status(200).json(data)
