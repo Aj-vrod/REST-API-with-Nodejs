b1d63ebc647cf365ce5fd74150bc5ccdd4878406
Ammy_Varela <ammyvrodriguez@hotmail.com>
2021-04-14 17:19:47 +0200

    added more tests for timespan to edit function to reject past days

--- tests/test.js
+++ tests/test.js
@@ -3,5 +3,8 @@ const testTimeSpanForNewBooking = require('../helpers/timespan')
 describe('bookings', () => {
   test('timespan', () => {
     expect(testTimeSpanForNewBooking('2021-04-20')).toBe(true)
+    expect(testTimeSpanForNewBooking('2021-04-30')).toBe(false)
+    expect(testTimeSpanForNewBooking('2021-04-16')).toBe(true)
+    expect(testTimeSpanForNewBooking('2021-04-10')).toBe(false)
   })
 })
