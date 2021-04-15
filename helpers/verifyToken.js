5e890c92de002978a73bfadf6d6366c3349ba44f
Ammy_Varela <ammyvrodriguez@hotmail.com>
2021-04-15 12:14:08 +0200

    added code to prevent crushing when no headers are provided

--- helpers/verifyToken.js
+++ helpers/verifyToken.js
@@ -1,19 +1,24 @@
 const jwt = require('jsonwebtoken')
 
+// INSERT TOKEN IN HEADER AS: BEARER <TOKEN> // BEARER eyJhbGciOiJIUzI1NiIsInR...
 const verifyToken = (req, res, next) => {
-  const bearerHeader = req.headers['authorization'];
-  if (typeof bearerHeader !== 'undefined') {
-    const bearerToken = bearerHeader.split(" ")[1];
-    req.token = bearerToken;
-    jwt.verify(bearerToken, 'secretToken', (err, user) => {
-      if (err) {
-        res.status(403).json({ "message": "403 Unauthorized action"})
-      }
-      req.userId = user.user[0].id
-      next();
-    })
+  if (req.headers['authorization']) {
+    const bearerHeader = req.headers['authorization'];
+    if (typeof bearerHeader !== 'undefined') {
+      const bearerToken = bearerHeader.split(" ")[1];
+      req.token = bearerToken;
+      jwt.verify(bearerToken, 'secretToken', (err, user) => {
+        if (err) {
+          res.status(403).json({ "message": "403 Unauthorized action"})
+        }
+        req.userId = user.user[0].id
+        next();
+      })
+    } else {
+      res.status(403).json({ "message": "403 Unauthorized action" })
+    }
   } else {
-    res.status(403).json({ "message": "403 Unauthorized action" })
+    res.status(403).json({ "message": "403 Unauthorized action"})
   }
 }
 
